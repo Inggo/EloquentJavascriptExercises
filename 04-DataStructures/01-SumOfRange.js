b2c0e883408bb94de34bc416f853189cd2a8890b
Inggo <inggo.espinosa@gmail.com>
2015-06-30 14:51:31 +0800

    Exercise 04-01 - The Sum Of A Range
    The first solution uses recursion which is slower and eventually fails on
    very large ranges.
    The second solution uses iteration which allows large ranges.
    
    http://eloquentjavascript.net/04_data.html#h_8ZspxiCEC/

---
+++ 04-DataStructures/01-SumOfRange.js
@@ -0,0 +1,31 @@
+// Uses recursion; faster on short ranges but slower and fails on very large ranges
+var range = function(start, end, step) {
+  // Set step if it is not defined
+  if (typeof step === "undefined") {
+    step = start < end ? 1 : -1;
+  }
+  // Determine if we have to stop the range
+  var stop = start == end ||
+      (start + step > end && step > 0) ||
+      (start + step < end && step < 0);
+  // Return an array with the starting element and recursively call the range,
+  // incrementing start by step
+  return [start].concat(stop ? [] : range(start + step, end, step));
+};
+
+var sum = function(arr) {
+  var result = 0;
+  for (var i = 0; i < arr.length; i++) {
+    result += arr[i];
+  }
+  return result;
+};
+
+console.log(range(1, 10));
+// → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(sum(range(1, 10)));
+// → 55
+console.log(range(5, 2, -1));
+// → [5, 4, 3, 2]
+console.log(range(5, 2, -2));
+// → [5, 3]
